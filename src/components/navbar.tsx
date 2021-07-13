1654078af853e06863ff39748a4b0c82d8c5fe92
cnb_interns <intern.cnbcomputers@gmail.com>
2021-07-13 13:49:33 -0700

    error in component

---
+++ src/components/navbar.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './components/navbar.css'
+
+const Navbar = () => {
+    return (
+        <div className="container">
+        <h1>CNB NET</h1>
+          <div className="dropdown">
+              <button className="dropbtn">Vendors
+                   <i className="fa fa-caret-down"></i>
+              </button>
+              <div className="dropdown-content">
+                  <a href="#">Link 1</a>
+                  <a href="#">Link 2</a>
+                  <a href="#">Link 3</a>
+                </div>
+                <button className="dropbtn">CNB
+                   <i className="fa fa-caret-down"></i>
+                </button>
+                <div className="dropdown-content">
+                    <a href="#">Link 1</a>
+                    <a href="#">Link 2</a>
+                    <a href="#">Link 3</a>
+                </div>
+                <div>Logout</div>
+            </div>
+        </div>
+        )
+}
+export default Navbar
