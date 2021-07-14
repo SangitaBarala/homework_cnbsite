813f506d4d063f2b4d87cb6737392c612bb3b7e6
cnb_interns <intern.cnbcomputers@gmail.com>
2021-07-14 15:52:14 -0400

    minor changer to Login and Register

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,22 @@
-<<<<<<< HEAD
+
 import React, { useState } from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Login from "./Authentication/Login";
+import Register from "./Authentication/Register";
+import Navbar1 from "./Authentication/Navbar1";
 
-import Login from "./Componants/Login";
-import Register from "./Componants/Register";
+
 
 function App() {
     
     return (<Router>
-        <div className="App">
-            <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-                <div className="container">
-                    <Link className="navbar-brand" to={"/sign-in"}>CNB Computers</Link>
-                    <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-                        <ul className="navbar-nav ml-auto">
-                            <li className="nav-item">
-                                <Link className="nav-link" to={"/sign-in"}>Login</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
+     
+        <Navbar1 />    
+     
+    </Router>);
 
-            <div className="auth-wrapper">
-                <div className="auth-inner">
-                    <Switch>
-                        <Route exact path='/' component={Login} />
-                        <Route path="/sign-in" component={Login} />
-                        <Route path="/sign-up" component={Register} />
-                    </Switch>
-                </div>
-            </div>
-        </div></Router>
-    );
-=======
-import Navbar from './components/navbar'
-import './App.css';
-
-
-function App() {
-  return (
-      <div>
-      <div className="App">
-          <Navbar/>
-      </div>
-
-    {/*<div className="App">*/}
-    {/*  <header className="App-header">*/}
-    {/*    < Login />*/}
-    {/*  </header>*/}
-          </div>
-         
-
-  );
->>>>>>> 96fdf13c9fa5191ca333e90b5ea1e2ac0625e878
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
@@ -14,14 +13,4 @@ ReactDOM.render(
 );
 
 reportWebVitals();
-=======
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
->>>>>>> 96fdf13c9fa5191ca333e90b5ea1e2ac0625e878
+
